138d7a2d49a078933d0335ccb51f3fc052855781
Supitpong Wongsaroj <ewrctysf@students.codeinstitute.net>
2024-11-23 08:17:33 +0000

    Add feature to addItemFromInventory function

--- run.py
+++ run.py
@@ -30,50 +30,49 @@ def get_user_choice():
         print("(6) Exit.                                            ")
         print("=====================================================")
 
-        choice = int(input("Enter your choice:"))
+        choice = int(input("Enter your choice by entering number 1 to 6:"))
+        print("\n")
 
         if choice == 1:
-
             viewInventory()
-
         elif choice == 2:
-
             addItemFromDelivery()
-
         elif choice == 3:
-
             removeItemFromUsage()
-
         elif choice == 4:
-
             removeItemFromWastage()
-
         elif choice == 5:
-
             redistribution()
-
         elif choice == 6:
-
             print("Exiting.")
             break
         else:
             print("Invalid choice. Please try again.")
 
 def viewInventory():
-
+    """
+    """
     inventory = SHEET.worksheet('inventory')
-
     inventory_data = inventory.get_all_values()
 
     for inventory_data in inventory_data:
         print(inventory_data)
+    
+    print("\n")
 
 def addItemFromDelivery():
-
     print("Please enter date and quantity of item recieve from delivery")
+    print("Data should begin with expiry date in the order of dd-mmm-yyy")
+    print("follow by =Today() and quantity to each hub ")
+    print("B1, Y1, R1, B2, Y2, R2 separated by commas.")
+    print("Example: 30-Nov-2024, =TODAY(), 6, 6, 6, 6, 6, 6\n")
+
+    delivery_input = input("Enter your data here: ")
+
+    delivery_data = delivery_input.split(",")
+    print(delivery_data)
 
 def removeItemFromUsage():
-
     print("Please enter date and quantity of item that has been used")
    
 
